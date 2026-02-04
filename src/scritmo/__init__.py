82996cea5caaf16e1cb41148f93b17ff1343be53
AndreaSalati <andrea.salati96@gmail.com>
2026-02-04 10:24:38 +0100

    preparing for big merge

--- src/scritmo/__init__.py
+++ src/scritmo/__init__.py
@@ -8,13 +8,3 @@ from .glm import glm_gene_fit
 from .beta import Beta, cSVD_beta, plot_beta_shift
 from .pychiral import CHIRAL
 from .dryseq.dryseq_main import dryseq
-
-
-# try:
-#     import jax
-#     import numpyro
-#     from .jax_module import *
-# # except ImportError:
-# #     pass
-# except Exception as e:
-#     pass
--- src/scritmo/jax_module/__init__.py
+++
@@ -1,3 +0,0 @@
-from .numpyro_models import model_MLE_NB, guide_MLE
-from .RITMO import RITMO
-from .simulations import simulate_dataset
